686b2c7e5b4fee7b50d2938296ae4da935d97a61
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-29 08:56:48 +0200

    Create add functions to add a guess to miss or hits MapSet

--- lib/island_engine/guesses.ex
+++ lib/island_engine/guesses.ex
@@ -1,12 +1,23 @@
 defmodule IslandEngine.Guesses do
   @moduledoc """
-    Provides API for make a guess
+    Provides API for create a Guess, add a guess to hit or miss MapSet
   """
 
   alias __MODULE__
+
+  alias IslandEngine.Coordinate
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new, do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
+  end
+
 end
